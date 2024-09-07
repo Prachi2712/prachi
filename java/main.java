31bc96c0edb7a0418229c494fa38327029ace24f
Prachi2712 <166316900+Prachi2712@users.noreply.github.com>
2024-09-07 10:31:51 +0530

    Update main.java

--- java/main.java
+++ java/main.java
@@ -6,7 +6,7 @@ public class main {
         // System.out.println("Enter your name ");
         // String name= sc.next();
         // System.out.println(name + " is a good girl");
-        System.out.println("Enter a full name ");
+        System.out.println("Enter a good name ");
         int num= sc.nextInt();
         System.out.println(num);
     }
